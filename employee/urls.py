a36dafba30840a40df449ad13a6f34eb0985a43f
WILSON ALBERTO MORALES LAZARO <wilsonm57wm@gmail.com>
2025-06-08 23:40:05 -0600

    [Update]: Se agregaron estilos a las lantillas

--- employee/urls.py
+++ employee/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EmployeeListView, EmployeeDetailView, EmployeeUpdateView, EmployeeCreateView
+from .views import EmployeeListView, EmployeeDetailView, EmployeeUpdateView, EmployeeCreateView, EmployeeDeleteView
 app_name = 'employee'
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create/', EmployeeCreateView.as_view(), name='employee_create'),
     path('<int:pk>/', EmployeeDetailView.as_view(), name='employee_detail'),
     path('<int:pk>/update/', EmployeeUpdateView.as_view(), name='employee_update'),
-
+    path('<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee_delete'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Employee
-from django.views.generic import ListView, UpdateView, DetailView, CreateView
+from django.views.generic import ListView, UpdateView, DetailView, CreateView, DeleteView
 from .forms import EmployeeForm
 from django.contrib import messages
 from django.urls import reverse, reverse_lazy
@@ -28,8 +28,8 @@ class EmployeeUpdateView(UpdateView):
     form_class = EmployeeForm
 
     def get_success_url(self):
-        messages.success(self.request, "Cliente actualizado exitosamente.", extra_tags='success')
-        return reverse('clients:client_detail', kwargs={'pk': self.object.pk})
+        messages.success(self.request, "Empleado actualizado exitosamente.", extra_tags='success')
+        return reverse('employee:employee_detail', kwargs={'pk': self.object.pk})
 
 class EmployeeCreateView(CreateView):
     model = Employee
@@ -37,5 +37,14 @@ class EmployeeCreateView(CreateView):
     form_class = EmployeeForm
 
     def get_success_url(self):
-        messages.success(self.request, "Cliente creado exitosamente.", extra_tags='success')
-        return reverse('clients:client_detail', kwargs={'pk': self.object.pk})
+        messages.success(self.request, "Empleado creado exitosamente.", extra_tags='success')
+        return reverse('employee:employee_detail', kwargs={'pk': self.object.pk})
+    
+class EmployeeDeleteView(DeleteView):
+    model = Employee
+    template_name = 'employee/employee_delete.html'
+    context_object_name = 'employee'
+
+    def get_success_url(self):
+        messages.success(self.request, "Empleado eliminado exitosamente.", extra_tags='success')
+        return reverse_lazy('employee:employee_list')
