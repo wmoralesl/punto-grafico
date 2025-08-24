fff6418a590de7bfd0c598a59b3a6fa325dac9ef
WILSON ALBERTO MORALES LAZARO <wilsonm57wm@gmail.com>
2025-08-24 17:03:38 -0600

    [Update]: Correcciones necesarias para la migracion de datos

--- clients/models.py
+++ clients/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Client(models.Model):
     name = models.CharField(max_length=256)
-    phone = models.PositiveBigIntegerField(unique=True)
+    phone = models.PositiveBigIntegerField(unique=True, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)    
     updated= models.DateTimeField(auto_now=True)
 
